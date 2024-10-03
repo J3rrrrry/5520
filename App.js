ce6419ddc2f1e513e3a550ab561787a16b7f5c4f
J3rrrrry <zhuoyuanliu518@gmail.com>
2024-10-03 11:49:38 -0700

    Change title and background colour in home screen

--- App.js
+++ App.js
@@ -3,14 +3,20 @@ import Home from "./Components/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GoalDetails from "./Components/GoalDetails";
-
 const Stack = createNativeStackNavigator();
-
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerStyle: { backgroundColor: "purple" },
+            headerTintColor: "white",
+            title: "My Goals",
+          }}
+        />
         <Stack.Screen name="Details" component={GoalDetails} />
       </Stack.Navigator>
     </NavigationContainer>
